886499b24bf207ad39a7a1e48c2fbbf35a5bde3c
TUDjose <J.B.PintodeMouraLeitedaCunha@student.tudelft.nl>
2023-12-06 20:30:51 +0100

    refactored model to be able to run it repeatedly

--- Model/model.py
+++ Model/model.py
@@ -5,222 +5,210 @@ Create a MILP forumlation for a repatriation scheduling problem based on the fol
 
 import gurobipy as gp
 import numpy as np
-import csv
-import pandas as pd
-
-# read data from csv file
-df = np.genfromtxt('test_data.csv', delimiter=';', skip_header=1)
-
-# Create a new model
-model = gp.Model()
-
-# SETS
-M = 5     # set of cities
-N = 4     # set of priority groups
-U = 10    # set of airplanes
-
-# PROBLEM INPUTS
-C = np.empty((U, ))      # data structure for capacity of airplane in k ∈ U
-P = np.empty((M, N))     # data structure for number of individuals of group j ∈ N in city i ∈ M
-QC = 2500                # number of available quarantine locations
-w = np.empty((N, ))      # importance of citizens in group j ∈ N
-
-def set_airplane_capacity():
-    C.fill(300)
-
-def set_individuals():
-    for i in range(M):
-        for j in range(N):
-            P[i][j] = df[i][j]
-
-def set_importance():
-    for j in range(N):
-        w[j] = df[-1][j]
-
-set_airplane_capacity()
-set_individuals()
-set_importance()
+from tqdm import tqdm
 
 
-# MILP VARIABLES
+class RSPModel:
+    def __init__(self, data, w, U=2, QC=2000):
+        self.model = gp.Model()
+        self.P = data
+        self.M, self.N = self.P.shape
+        self.w = w
+        self.U = U
+        self.C = np.empty((self.U,))
+        self.QC = QC
+        self.V = 10e6
 
-alpha = {}
-for i in range(M):
-    for j in range(N):
-        alpha[i, j] = model.addVar(vtype=gp.GRB.BINARY, name="alpha[%s,%s]"%(i,j))
+        self.alpha = {}
+        self.epsilon = {}
+        self.L = {}
+        self.P_cum = {}
+        self.P_hat_cum = {}
+        self.R = {}
+        self.x = {}
 
-epsilon = {}
-for i in range(M):
-    epsilon[i] = model.addVar(vtype=gp.GRB.BINARY, name="epsilon[%s]"%(i))
+    def __repr__(self):
+        return f'RSPModel({self.P}, {self.w}, {self.U}, {self.QC})'
 
-L = {}
-for i in range(M):
-    L[i] = model.addVar(vtype=gp.GRB.INTEGER, name="L[%s]"%(i))
+    def set_airplane_capacity(self):
+        self.C.fill(300)
 
-P_cum = {}
-for i in range(M):
-    for j in range(N):
-        # P_cum[i, j] = model.addVar(vtype=gp.GRB.INTEGER, name="P_cum[%s,%s]"%(i,j))
-        P_cum[i, j] = gp.quicksum(P[i, u] for u in range(j+1))  # define P_cum directly instead of as a variable/constraint
+    def create_variables(self):
+        for i in range(self.M):
+            self.epsilon[i] = self.model.addVar(vtype=gp.GRB.BINARY, name="epsilon[%s]"%(i))
 
-P_hat_cum = {}
-for i in range(M):
-    for j in range(N):
-        P_hat_cum[i, j] = model.addVar(vtype=gp.GRB.INTEGER, name="P_hat_cum[%s,%s]"%(i,j))
+        for i in range(self.M):
+            self.L[i] = self.model.addVar(vtype=gp.GRB.INTEGER, name="L[%s]" % (i))
 
-R = {}
-for i in range(M):
-    for j in range(N):
-        R[i, j] = model.addVar(vtype=gp.GRB.INTEGER, name="R[%s,%s]"%(i,j))
+        for i in range(self.M):
+            for j in range(self.N):
+                self.alpha[i, j] = self.model.addVar(vtype=gp.GRB.BINARY, name="alpha[%s,%s]"%(i,j))
 
-x = {}
-for i in range(M):
-    for k in range(U):
-        x[i, k] = model.addVar(vtype=gp.GRB.BINARY, name="xik[%s,%s]"%(i,k))
+        for i in range(self.M):
+            for j in range(self.N):
+                self.P_cum[i, j] = gp.quicksum(self.P[i, u] for u in range(j + 1))
 
-model.update()
+        for i in range(self.M):
+            for j in range(self.N):
+                self.P_hat_cum[i, j] = self.model.addVar(vtype=gp.GRB.INTEGER, name="P_hat_cum[%s,%s]" % (i, j))
+
+        for i in range(self.M):
+            for j in range(self.N):
+                self.R[i, j] = self.model.addVar(vtype=gp.GRB.INTEGER, name="R[%s,%s]" % (i, j))
+
+        for i in range(self.M):
+            for k in range(self.U):
+                self.x[i, k] = self.model.addVar(vtype=gp.GRB.BINARY, name="xik[%s,%s]"%(i,k))
+
+        self.model.update()
+
+    def create_objective(self):
+        self.obj = gp.LinExpr()
+        self.obj += gp.quicksum(self.w[j] * self.R[i, j] for i in range(self.M) for j in range(self.N))
+        self.model.setObjective(self.obj, gp.GRB.MAXIMIZE)
+        self.model.update()
+
+    def create_constraints(self):
+        # (3)
+        for i in range(self.M):
+            LHS, RHS = gp.LinExpr(), gp.LinExpr()
+            LHS += self.P_cum[i, self.N - 1] - gp.quicksum(self.C[k] * self.x[i, k] for k in range(self.U))
+            RHS += self.V * (self.epsilon[i] - 1)
+            self.model.addConstr(LHS >= RHS, name='(3)[%s]' % (i))
+
+        # (4)
+        for i in range(self.M):
+            LHS, RHS = gp.LinExpr(), gp.LinExpr()
+            LHS += self.P_cum[i, self.N - 1] - gp.quicksum(self.C[k] * self.x[i, k] for k in range(self.U))
+            RHS += self.V * self.epsilon[i]
+            self.model.addConstr(LHS <= RHS, name='(4)[%s]' % (i))
+
+        # (5)
+        for i in range(self.M):
+            LHS, RHS = gp.LinExpr(), gp.LinExpr()
+            LHS += self.L[i]
+            RHS += gp.quicksum(self.C[k] * self.x[i, k] for k in range(self.U)) + self.V * (1 - self.epsilon[i])
+            self.model.addConstr(LHS <= RHS, name='(5)[%s]' % (i))
+
+        # (6)
+        for i in range(self.M):
+            LHS, RHS = gp.LinExpr(), gp.LinExpr()
+            LHS += self.L[i]
+            RHS += gp.quicksum(self.C[k] * self.x[i, k] for k in range(self.U)) + self.V * (self.epsilon[i] - 1)
+            self.model.addConstr(LHS >= RHS, name='(6)[%s]' % (i))
+
+        # (7)
+        for i in range(self.M):
+            LHS, RHS = gp.LinExpr(), gp.LinExpr()
+            LHS += self.L[i]
+            RHS += self.P_cum[i, self.N - 1] + self.V * self.epsilon[i]
+            self.model.addConstr(LHS <= RHS, name='(7)[%s]' % (i))
+
+        # (8)
+        for i in range(self.M):
+            LHS, RHS = gp.LinExpr(), gp.LinExpr()
+            LHS += self.L[i]
+            RHS += self.P_cum[i, self.N - 1] - self.V * self.epsilon[i]
+            self.model.addConstr(LHS >= RHS, name='(8)[%s]' % (i))
+
+        # (9)
+        for i in range(self.M):
+            for j in range(self.N):
+                LHS, RHS = gp.LinExpr(), gp.LinExpr()
+                LHS += self.L[i] - self.P_cum[i, j]
+                RHS += self.V * (self.alpha[i, j] - 1)
+                self.model.addConstr(LHS >= RHS, name='(9)[%s,%s]' % (i, j))  # should be strict inequality
+
+        # (10)
+        for i in range(self.M):
+            for j in range(self.N):
+                LHS, RHS = gp.LinExpr(), gp.LinExpr()
+                LHS += self.L[i] - self.P_cum[i, j]
+                RHS += self.V * self.alpha[i, j]
+                self.model.addConstr(LHS <= RHS, name='(10)[%s,%s]' % (i, j))
+
+        # (11)
+        for i in range(self.M):
+            for j in range(self.N):
+                LHS, RHS = gp.LinExpr(), gp.LinExpr()
+                LHS += self.P_hat_cum[i, j]
+                RHS += (1 - self.alpha[i, j]) * self.V
+                self.model.addConstr(LHS <= RHS, name='(11)[%s,%s]' % (i, j))
+
+        # (12)
+        for i in range(self.M):
+            for j in range(self.N):
+                LHS, RHS = gp.LinExpr(), gp.LinExpr()
+                LHS += self.P_hat_cum[i, j]
+                RHS += (self.alpha[i, j] - 1) * self.V
+                self.model.addConstr(LHS >= RHS, name='(12)[%s,%s]' % (i, j))
+
+        # (13)
+        for i in range(self.M):
+            for j in range(self.N):
+                LHS, RHS = gp.LinExpr(), gp.LinExpr()
+                LHS += self.P_hat_cum[i, j]
+                RHS += self.P_cum[i, j] - self.L[i] + self.alpha[i, j] * self.V
+                self.model.addConstr(LHS <= RHS, name='(13)[%s,%s]' % (i, j))
+
+        # (14)
+        for i in range(self.M):
+            for j in range(self.N):
+                LHS, RHS = gp.LinExpr(), gp.LinExpr()
+                LHS += self.P_hat_cum[i, j]
+                RHS += self.P_cum[i, j] - self.L[i] - self.alpha[i, j] * self.V
+                self.model.addConstr(LHS >= RHS, name='(14)[%s,%s]' % (i, j))
+
+        # (15)
+        for i in range(self.M):
+            for j in range(self.N):
+                LHS, RHS = gp.LinExpr(), gp.LinExpr()
+                LHS += self.R[i, j]
+                if j != 0:
+                    RHS += self.P_cum[i, j] - self.P_hat_cum[i, j] - self.P_cum[i, j - 1] + self.P_hat_cum[i, j - 1]
+                elif j == 0:
+                    RHS += self.P_cum[i, j] - self.P_hat_cum[i, j]
+                self.model.addConstr(LHS == RHS, name='(15)[%s,%s]' % (i, j))
+
+        # (16)
+        self.model.addConstr(gp.quicksum(self.L[i] for i in range(self.M)) <= self.QC, name='(16)')
+
+        # (17)
+        for k in range(self.U):
+            self.model.addConstr(gp.quicksum(self.x[i, k] for i in range(self.M)) <= 1, name='(17)[%s]' % (k))
+
+        self.model.update()
+
+    def solve(self):
+        self.model.Params.LogToConsole = 0
+
+        self.set_airplane_capacity()
+        self.create_variables()
+        self.create_objective()
+        self.create_constraints()
+
+        self.model.update()
+        self.model.optimize()
+        if self.model.status == gp.GRB.INF_OR_UNBD or self.model.status == gp.GRB.INFEASIBLE:
+            self.model.computeIIS()
+            print('\nThe following constraint(s) cannot be satisfied:')
+            for c in self.model.getConstrs():
+                if c.IISConstr:
+                    print('%s' % c.constrName)
+        else:
+            self.model.write(f'results/model_{self.U}_{self.QC}.lp')
 
 
-# OBJECTIVE FUNCTION
-# (1)
-obj = gp.LinExpr()
-obj += gp.quicksum(w[j] * R[i, j] for i in range(M) for j in range(N))
-model.setObjective(obj, gp.GRB.MAXIMIZE)
-model.update()
+if __name__ == '__main__':
+    us = [2,4,6,8,8,10,10,12,12,12,14]
+    QCs = [2000,2000,2000,2000,2500,2500,3000,3000,3500,4000,4000]
+    df = np.genfromtxt('test_data.csv', delimiter=';', skip_header=1)
 
-# CONSTRAINTS
+    results = []
 
-V = 10e6    # big M
+    for i in tqdm(range(len(us))):
+        RSP = RSPModel(data=df[:5,:], w=df[5,:], U=us[i], QC=QCs[i])
+        RSP.solve()
+        results.append([RSP.U, RSP.QC, RSP.model.objVal])
 
-# (2)
-# for i in range(M):
-#     for j in range(N):
-#         model.addConstr(P_cum[i, j] == gp.quicksum(P[i, u] for u in range(j+1)), name='(2)[%s,%s]'%(i,j))
-
-# (3)
-for i in range(M):
-    LHS, RHS = gp.LinExpr(), gp.LinExpr()
-    LHS += P_cum[i, N-1] - gp.quicksum(C[k] * x[i, k] for k in range(U))
-    RHS += V * (epsilon[i] - 1)
-    model.addConstr(LHS >= RHS, name='(3)[%s]'%(i))
-
-# (4)
-for i in range(M):
-    LHS, RHS = gp.LinExpr(), gp.LinExpr()
-    LHS += P_cum[i, N-1] - gp.quicksum(C[k] * x[i, k] for k in range(U))
-    RHS += V * epsilon[i]
-    model.addConstr(LHS <= RHS, name='(4)[%s]'%(i))
-
-# (5)
-for i in range(M):
-    LHS, RHS = gp.LinExpr(), gp.LinExpr()
-    LHS += L[i]
-    RHS += gp.quicksum(C[k] * x[i, k] for k in range(U)) + V * (1 - epsilon[i])
-    model.addConstr(LHS <= RHS, name='(5)[%s]'%(i))
-
-# (6)
-for i in range(M):
-    LHS, RHS = gp.LinExpr(), gp.LinExpr()
-    LHS += L[i]
-    RHS += gp.quicksum(C[k] * x[i, k] for k in range(U)) + V * (epsilon[i] - 1)
-    model.addConstr(LHS >= RHS, name='(6)[%s]'%(i))
-
-# (7)
-for i in range(M):
-    LHS, RHS = gp.LinExpr(), gp.LinExpr()
-    LHS += L[i]
-    RHS += P_cum[i, N-1] + V * epsilon[i]
-    model.addConstr(LHS <= RHS, name='(7)[%s]'%(i))
-
-# (8)
-for i in range(M):
-    LHS, RHS = gp.LinExpr(), gp.LinExpr()
-    LHS += L[i]
-    RHS += P_cum[i, N-1] - V * epsilon[i]
-    model.addConstr(LHS >= RHS, name='(8)[%s]'%(i))
-
-# (9)
-for i in range(M):
-    for j in range(N):
-        LHS, RHS = gp.LinExpr(), gp.LinExpr()
-        LHS += L[i] - P_cum[i, j]
-        RHS += V * (alpha[i, j] - 1)
-        model.addConstr(LHS >= RHS, name='(9)[%s,%s]'%(i,j))    # should be strict inequality
-
-# (10)
-for i in range(M):
-    for j in range(N):
-        LHS, RHS = gp.LinExpr(), gp.LinExpr()
-        LHS += L[i] - P_cum[i, j]
-        RHS += V * alpha[i, j]
-        model.addConstr(LHS <= RHS, name='(10)[%s,%s]'%(i,j))
-
-# (11)
-for i in range(M):
-    for j in range(N):
-        LHS, RHS = gp.LinExpr(), gp.LinExpr()
-        LHS += P_hat_cum[i, j]
-        RHS += (1 - alpha[i, j]) * V
-        model.addConstr(LHS <= RHS, name='(11)[%s,%s]'%(i,j))
-
-# (12)
-for i in range(M):
-    for j in range(N):
-        LHS, RHS = gp.LinExpr(), gp.LinExpr()
-        LHS += P_hat_cum[i, j]
-        RHS += (alpha[i, j] - 1) * V
-        model.addConstr(LHS >= RHS, name='(12)[%s,%s]'%(i,j))
-
-# (13)
-for i in range(M):
-    for j in range(N):
-        LHS, RHS = gp.LinExpr(), gp.LinExpr()
-        LHS += P_hat_cum[i, j]
-        RHS += P_cum[i, j] - L[i] + alpha[i, j] * V
-        model.addConstr(LHS <= RHS, name='(13)[%s,%s]'%(i,j))
-
-# (14)
-for i in range(M):
-    for j in range(N):
-        LHS, RHS = gp.LinExpr(), gp.LinExpr()
-        LHS += P_hat_cum[i, j]
-        RHS += P_cum[i, j] - L[i] - alpha[i, j] * V
-        model.addConstr(LHS >= RHS, name='(14)[%s,%s]'%(i,j))
-
-# (15)
-for i in range(M):
-    for j in range(N):
-        LHS, RHS = gp.LinExpr(), gp.LinExpr()
-        LHS += R[i, j]
-        if j != 0:
-            RHS += P_cum[i, j] - P_hat_cum[i, j] - P_cum[i, j-1] + P_hat_cum[i, j-1]
-        elif j == 0:
-            RHS += P_cum[i, j] - P_hat_cum[i, j]
-        model.addConstr(LHS == RHS, name='(15)[%s,%s]'%(i,j))
-
-# (16)
-model.addConstr(gp.quicksum(L[i] for i in range(M)) <= QC, name='(16)')
-
-# (17)
-for k in range(U):
-    model.addConstr(gp.quicksum(x[i, k] for i in range(M)) <= 1, name='(17)[%s]'%(k))
-
-
-model.update()
-model.optimize()
-model.write('model.lp')
-
-if model.status == gp.GRB.INF_OR_UNBD or model.status == gp.GRB.INFEASIBLE:
-    model.computeIIS()
-    model.write("infeasible.lp")
-    print('\nThe following constraint(s) cannot be satisfied:')
-    for c in model.getConstrs():
-        if c.IISConstr:
-            print('%s' % c.constrName)
-
-for (i, j) in R.keys():
-    print(f'{i},{j}: ', R[i, j].X)
-
-for (i, k) in x.keys():
-    print(f'{i},{k}: ', x[i, k].X)
-print(sum(x[i, k].X for (i, k) in x.keys()))
+    print(results)
