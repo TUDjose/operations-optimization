3849c5432c3a61d2723c5b9c37f03d1ddc8e23b2
TUDjose <J.B.PintodeMouraLeitedaCunha@student.tudelft.nl>
2023-12-13 11:32:15 +0100

    structure of unittests

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -1,5 +1,60 @@
 import unittest
 from Model.model import RSPModel
+from Model.data_generator import DataGenerator
 
 class TestConstraints(unittest.TestCase):
-    pass
+
+    dg = DataGenerator.from_file('../tests/test_data.csv', 2, 2000)
+    RSP = RSPModel(dg)
+    RSP.solve()
+
+    def test_objetive(self):
+        pass
+
+    def test_c2(self):
+        pass
+
+    def test_c3(self):
+        pass
+
+    def test_c4(self):
+        pass
+
+    def test_c5(self):
+        pass
+
+    def test_c6(self):
+        pass
+
+    def test_c7(self):
+        pass
+
+    def test_c8(self):
+        pass
+
+    def test_c9(self):
+        pass
+
+    def test_c10(self):
+        pass
+
+    def test_c11(self):
+        pass
+
+    def test_c12(self):
+        pass
+
+    def test_c13(self):
+        pass
+
+    def test_c14(self):
+        pass
+
+    def test_c15(self):
+        pass
+
+    def test_c16(self):
+        pass
+
+    def test_c17(self):
+        pass
